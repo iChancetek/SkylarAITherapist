0e5478cd89240ab6c7847dfd3a670bc8d91177d9
Synera <isynera@gmail.com>
2025-07-22 21:54:01 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "skylar-ai-voice-therapy",
+  "appId": "1:584729201545:web:cf415a8466017b55f56f58",
+  "storageBucket": "skylar-ai-voice-therapy.firebasestorage.app",
+  "apiKey": "AIzaSyAoIOCJlx30-KHjb5MKU51GibpUBpazrWo",
+  "authDomain": "skylar-ai-voice-therapy.firebaseapp.com",
+  "messagingSenderId": "584729201545"
 };
 
 // Initialize Firebase
